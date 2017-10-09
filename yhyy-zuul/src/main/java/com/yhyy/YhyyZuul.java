4e1b210e2076c5160d7701906400e947e738adc4
ww <wenwu@shuangchuangpuhui.com>
2017-10-09 17:42:38 +0800

    悦花越有网关

---
+++ yhyy-zuul/src/main/java/com/yhyy/YhyyZuul.java
@@ -0,0 +1,15 @@
+package com.yhyy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableFeignClients
+public class YhyyZuul {
+    public static void main(String[] args) {
+        SpringApplication.run(YhyyZuul.class, args);
+    }
+}
